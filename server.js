9c0c2b86c0208a0697dbc22bc26733b95f30222c
Ragulprabhu196 <123489882+Ragulprabhu196@users.noreply.github.com>
2023-06-26 15:39:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require ('express')
 const app = express()
-app.get("/api", (req,res)=>{
-    res.json({"name":["RAGUL PRABHU"],
-    "file name":["dom.docx"]})
-})
+
 app.listen(5000,()=>{console.log('SERVER STARTED SUCESSFULL')})
